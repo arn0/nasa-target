ca7260412fecd468a43c2db44e9232055054461f
arn0 <arnoc@xs4all.nl>
2024-01-23 00:29:59 +0100

    SPI configuration and segment decode

--- main/app_main.c
+++ main/app_main.c
@@ -25,45 +25,36 @@ SPI receiver (slave) example.
 This example is supposed to work together with the SPI sender. It uses the standard SPI pins (MISO, MOSI, SCLK, CS) to
 transmit data over in a full-duplex fashion, that is, while the master puts data on the MOSI pin, the slave puts its own
 data on the MISO pin.
-
-This example uses one extra pin: GPIO_HANDSHAKE is used as a handshake pin. After a transmission has been set up and we're
-ready to send/receive data, this code uses a callback to set the handshake pin high. The sender will detect this and start
-sending a transaction. As soon as the transaction is done, the line gets set low again.
 */
 
 /*
 Pins in use. The SPI Master can use the GPIO mux, so feel free to change these if needed.
 */
 #if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
-#define GPIO_HANDSHAKE 2
 #define GPIO_MOSI 12
 #define GPIO_MISO 13
 #define GPIO_SCLK 15
 #define GPIO_CS 14
 
 #elif CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32C2
-#define GPIO_HANDSHAKE 3
 #define GPIO_MOSI 7
 #define GPIO_MISO 2
 #define GPIO_SCLK 6
 #define GPIO_CS 10
 
 #elif CONFIG_IDF_TARGET_ESP32C6
-#define GPIO_HANDSHAKE 15
 #define GPIO_MOSI 19
 #define GPIO_MISO 20
 #define GPIO_SCLK 18
 #define GPIO_CS 9
 
 #elif CONFIG_IDF_TARGET_ESP32H2
-#define GPIO_HANDSHAKE 2
 #define GPIO_MOSI 5
 #define GPIO_MISO 0
 #define GPIO_SCLK 4
 #define GPIO_CS 1
 
 #elif CONFIG_IDF_TARGET_ESP32S3
-#define GPIO_HANDSHAKE 2
 #define GPIO_MOSI 11
 #define GPIO_MISO 13
 #define GPIO_SCLK 12
@@ -81,21 +72,284 @@ Pins in use. The SPI Master can use the GPIO mux, so feel free to change these i
 #endif
 
 
+const char *bit_rep[16] = {
+    [ 0] = "0000", [ 1] = "0001", [ 2] = "0010", [ 3] = "0011",
+    [ 4] = "0100", [ 5] = "0101", [ 6] = "0110", [ 7] = "0111",
+    [ 8] = "1000", [ 9] = "1001", [10] = "1010", [11] = "1011",
+    [12] = "1100", [13] = "1101", [14] = "1110", [15] = "1111",
+};
 
-//Called after a transaction is queued and ready for pickup by master. We use this to set the handshake line high.
-void my_post_setup_cb(spi_slave_transaction_t *trans) {
-    gpio_set_level(GPIO_HANDSHAKE, 1);
+void print_byte(uint8_t byte)
+{
+    printf("%s %s ", bit_rep[byte >> 4], bit_rep[byte & 0x0F]);
 }
 
-//Called after transaction is sent/received. We use this to set the handshake line low.
-void my_post_trans_cb(spi_slave_transaction_t *trans) {
-    gpio_set_level(GPIO_HANDSHAKE, 0);
+int segment0decode(char *buffer)
+{
+    char segment = (((buffer[1] & 0x07) << 4 ) | ((buffer[2] & 0xF0) >> 4));
+
+    switch (segment)
+    {
+    case 0b00000000:    // blank
+        return(0);
+        break;
+    
+    case 0b00100000:    // Upper or Shallow Alarm
+        return('S');
+        break;
+    
+    case 0b00000001:    // Lower or Deep Alarm
+        return('D');
+        break;
+    
+    case 0b01111010:    // A = Alarm on
+        return('A');
+        break;
+    
+    case 0b00001001:    // sensitivity threshold
+        return('T');
+        break;
+    
+    case 0b00111011:
+        return('E');
+        break;
+    
+    case 0b00101000:
+        return('K');     // Keel offset
+        break;
+    
+   default:
+        printf("segment0decode(): error decoding bits: %s %s ", bit_rep[segment >> 4], bit_rep[segment & 0x0F]);
+        return(-1);
+        break;
+    }
 }
+
+int segment1number(char *buffer)
+{
+    char segment = ((buffer[1] & 0xF0) | ((buffer[2] & 0x07) << 1));
+
+    switch (segment)
+    {
+    case 0b00000000:    // blank
+        return(0);
+        break;
+    
+    case 0b11101110:
+        return(0);
+        break;
+    
+    case 0b10000010:
+        return(1);
+        break;
+    
+    case 0b11011100:
+        return(2);
+        break;
+    
+    case 0b11010110:
+        return(3);
+        break;
+    
+    case 0b10110010:
+        return(4);
+        break;
+    
+    case 0b01110110:
+        return(5);
+        break;
+    
+    case 0b01111110:
+        return(6);
+        break;
+    
+    case 0b11000010:
+        return(7);
+        break;
+    
+    case 0b11111110:
+        return(8);
+        break;
+    
+    case 0b11110110:
+        return(9);
+        break;
+    
+   default:
+        printf("segment1number(): error decoding bits: %s %s ", bit_rep[segment >> 4], bit_rep[segment & 0x0F]);
+        return(-1);
+        break;
+    }
+}
+
+int segment2number(char *buffer)
+{
+    char segment = ((buffer[0] & 0x1E) << 3) | ((buffer[3] & 0xE0) >> 4);
+
+    switch (segment)
+    {
+    case 0b00000000:    // blank
+        return(0);
+        break;
+    
+    case 0b11101110:
+        return(0);
+        break;
+    
+    case 0b10000010:
+        return(1);
+        break;
+    
+    case 0b11011100:
+        return(2);
+        break;
+    
+    case 0b11010110:
+        return(3);
+        break;
+    
+    case 0b10110010:
+        return(4);
+        break;
+    
+    case 0b01110110:
+        return(5);
+        break;
+    
+    case 0b01111110:
+        return(6);
+        break;
+    
+    case 0b11000010:
+        return(7);
+        break;
+    
+    case 0b11111110:
+        return(8);
+        break;
+    
+    case 0b11110110:
+        return(9);
+        break;
+    
+   default:
+        printf("segment2number(): error decoding bits: %s %s ", bit_rep[segment >> 4], bit_rep[segment & 0x0F]);
+        return(-1);
+        break;
+    }
+}
+int segment3number(char *buffer)
+{
+    char segment = ((buffer[0] & 0xE0) | (buffer[3] & 0x0F));
+
+    switch (segment)
+    {
+    case 0b00000000:    // blank
+        return(0);
+        break;
+    
+    case 0b11001111:
+        return(0);
+        break;
+    
+    case 0b00000011:
+        return(1);
+        break;
+    
+    case 0b10101101:
+        return(2);
+        break;
+    
+    case 0b10100111:
+        return(3);
+        break;
+    
+    case 0b01100011:
+        return(4);
+        break;
+    
+    case 0b11100110:
+        return(5);
+        break;
+    
+    case 0b11101110:
+        return(6);
+        break;
+    
+    case 0b10000011:
+        return(7);
+        break;
+    
+    case 0b11101111:
+        return(8);
+        break;
+    
+    case 0b11100111:
+        return(9);
+        break;
+    
+   default:
+        printf("segment3number(): error decoding bits: %s %s ", bit_rep[segment >> 4], bit_rep[segment & 0x0F]);
+        return(-1);
+        break;
+    }
+}
+
+void app_decode_segments(char *buffer)
+{
+    int i;
+    float number;
+
+//    print_byte(buffer[0]);
+//    print_byte(buffer[1]);
+//    print_byte(buffer[2]);
+//    print_byte(buffer[3]);
+
+    i = segment1number(buffer);
+    if(i != -1){
+        number = i * 10;
+        i = segment2number(buffer);
+        if(i != -1){
+            number += i;
+            i = segment3number(buffer);
+            if(i != -1){
+                number += (i / 10.0);
+                printf("  %02.1f\n", number );
+            }
+        }
+    }
+    i = segment0decode(buffer);
+    switch(i)
+    {
+    case 'S':
+        printf("Set shallow alarm\n");
+        break;
+
+    case 'D':
+        printf("Set deep alarm\n");
+        break;
+
+    case 'A':
+        printf("Alarm on\n");
+        break;
+
+    case 'T':
+        printf("Set sensitivity threshold\n");
+        break;
+
+    case 'K':
+        printf("Set keel offset\n");
+        break;
+
+    default:
+        break;
+    }
+}
+
 
 //Main application
 void app_main(void)
 {
-    int n=0;
     esp_err_t ret;
 
     //Configuration for the SPI bus
@@ -109,59 +363,43 @@ void app_main(void)
 
     //Configuration for the SPI slave interface
     spi_slave_interface_config_t slvcfg={
-        .mode=0,
+        .mode=3,
         .spics_io_num=GPIO_CS,
-        .queue_size=3,
+        .queue_size=8,
         .flags=0,
-        .post_setup_cb=my_post_setup_cb,
-        .post_trans_cb=my_post_trans_cb
     };
-
-    //Configuration for the handshake line
-    gpio_config_t io_conf={
-        .intr_type=GPIO_INTR_DISABLE,
-        .mode=GPIO_MODE_OUTPUT,
-        .pin_bit_mask=(1<<GPIO_HANDSHAKE)
-    };
-
-    //Configure handshake line as output
-    gpio_config(&io_conf);
-    //Enable pull-ups on SPI lines so we don't detect rogue pulses when no master is connected.
-    gpio_set_pull_mode(GPIO_MOSI, GPIO_PULLUP_ONLY);
-    gpio_set_pull_mode(GPIO_SCLK, GPIO_PULLUP_ONLY);
-    gpio_set_pull_mode(GPIO_CS, GPIO_PULLUP_ONLY);
 
     //Initialize SPI slave interface
-    ret=spi_slave_initialize(RCV_HOST, &buscfg, &slvcfg, SPI_DMA_CH_AUTO);
+    ret=spi_slave_initialize(RCV_HOST, &buscfg, &slvcfg, SPI_DMA_DISABLED);
     assert(ret==ESP_OK);
 
-    WORD_ALIGNED_ATTR char sendbuf[129]="";
-    WORD_ALIGNED_ATTR char recvbuf[129]="";
-    memset(recvbuf, 0, 33);
+    //WORD_ALIGNED_ATTR char sendbuf[129]="";
+    WORD_ALIGNED_ATTR char recvbuf[8];
+    memset(recvbuf, 0, sizeof(recvbuf));
     spi_slave_transaction_t t;
     memset(&t, 0, sizeof(t));
 
     while(1) {
-        //Clear receive buffer, set send buffer to something sane
-        memset(recvbuf, 0xA5, 129);
-        sprintf(sendbuf, "This is the receiver, sending data for transmission number %04d.", n);
+        //Clear receive buffer
+        memset(recvbuf, 0, sizeof(recvbuf));
+//        memset(recvbuf, 0xA5, 129);
 
         //Set up a transaction of 128 bytes to send/receive
-        t.length=128*8;
-        t.tx_buffer=sendbuf;
+        t.length=4*8;
+        t.tx_buffer=NULL;
         t.rx_buffer=recvbuf;
         /* This call enables the SPI slave interface to send/receive to the sendbuf and recvbuf. The transaction is
         initialized by the SPI master, however, so it will not actually happen until the master starts a hardware transaction
-        by pulling CS low and pulsing the clock etc. In this specific example, we use the handshake line, pulled up by the
-        .post_setup_cb callback that is called as soon as a transaction is ready, to let the master know it is free to transfer
-        data.
+        by pulling CS low and pulsing the clock etc.
         */
         ret=spi_slave_transmit(RCV_HOST, &t, portMAX_DELAY);
 
         //spi_slave_transmit does not return until the master has done a transmission, so by here we have sent our data and
         //received data from the master. Print it.
-        printf("Received: %s\n", recvbuf);
-        n++;
+
+        app_decode_segments(recvbuf);
+
+        //printf("\n                         ^\n");
     }
 
 }
