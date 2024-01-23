0958c62801fca2e8ae28826afdb2619fee487f9d
arn0 <arnoc@xs4all.nl>
2024-01-23 21:01:34 +0100

    Add blinking LED

--- main/app_main.c
+++ main/app_main.c
@@ -71,6 +71,64 @@ Pins in use. The SPI Master can use the GPIO mux, so feel free to change these i
 
 #endif
 
+/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
+   or you can edit the following line and set a number here.
+*/
+#define BLINK_GPIO CONFIG_BLINK_GPIO
+
+static uint8_t s_led_state = 0;
+
+#ifdef CONFIG_BLINK_LED_RMT
+
+static led_strip_handle_t led_strip;
+
+static void blink_led(void)
+{
+    /* If the addressable LED is enabled */
+    if (s_led_state) {
+        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
+        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
+        /* Refresh the strip to send data */
+        led_strip_refresh(led_strip);
+    } else {
+        /* Set all LED off to clear all pixels */
+        led_strip_clear(led_strip);
+    }
+}
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
+    /* LED strip initialization with the GPIO and pixels number*/
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = BLINK_GPIO,
+        .max_leds = 1, // at least one LED on board
+    };
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    /* Set all LED off to clear all pixels */
+    led_strip_clear(led_strip);
+}
+
+#elif CONFIG_BLINK_LED_GPIO
+
+static void blink_led(void)
+{
+    /* Set the GPIO level according to the state (LOW or HIGH)*/
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+}
+
+#endif
 
 const char *bit_rep[16] = {
     [ 0] = "0000", [ 1] = "0001", [ 2] = "0010", [ 3] = "0011",
@@ -379,10 +437,16 @@ void app_main(void)
     spi_slave_transaction_t t;
     memset(&t, 0, sizeof(t));
 
+    /* Configure the peripheral according to the LED type */
+    configure_led();
+
     while(1) {
         //Clear receive buffer
         memset(recvbuf, 0, sizeof(recvbuf));
 //        memset(recvbuf, 0xA5, 129);
+
+        s_led_state = 0;
+        blink_led();
 
         //Set up a transaction of 128 bytes to send/receive
         t.length=4*8;
@@ -393,6 +457,9 @@ void app_main(void)
         by pulling CS low and pulsing the clock etc.
         */
         ret=spi_slave_transmit(RCV_HOST, &t, portMAX_DELAY);
+
+        s_led_state = 1;
+        blink_led();
 
         //spi_slave_transmit does not return until the master has done a transmission, so by here we have sent our data and
         //received data from the master. Print it.
